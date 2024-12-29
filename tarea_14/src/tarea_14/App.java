998aebc0f060bb50820b9f97bc042a05415c734f
beton <beton@i7-6700K-Betonick>
2024-12-29 22:27:20 +0100

    Estableciendo la conexión

--- tarea_14/src/tarea_14/App.java
+++ tarea_14/src/tarea_14/App.java
@@ -1,9 +1,27 @@
 package tarea_14;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import pool.MyDataSource;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		try (Connection conn = MyDataSource.getConnection()) {
+			DatabaseMetaData metaData = conn.getMetaData();
+			String[] types = { "TABLE" };
+			ResultSet tables = metaData.getTables(null, null, "%", types);
+			while (tables.next()) {
+				System.out.println(tables.getString("TABLE_NAME"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 	}
 
